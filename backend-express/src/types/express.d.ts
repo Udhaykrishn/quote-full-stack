2216bb155c7243d1ac3b68313975b16f265c2082
UdhayKrishn <krishnauday320@gmail.com>
2026-02-20 17:48:27 +0530

    docs(backend): add inline documentation for express.d.ts

---
+++ backend-express/src/types/express.d.ts
@@ -0,0 +1,19 @@
+import { Session } from "@shopify/shopify-api";
+
+declare global {
+    namespace Express {
+        interface Request {
+            merchantId?: string;
+            shopify?: {
+                shop: string;
+            };
+        }
+        interface Locals {
+            shopify: {
+                session: Session;
+            };
+        }
+    }
+}
+
+export { };
