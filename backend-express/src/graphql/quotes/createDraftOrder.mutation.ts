dc3d06f6d2ca58bf9684f68d5bb6c52203e0e590
UdhayKrishn <krishnauday320@gmail.com>
2026-02-21 10:40:49 +0530

    chore(backend): setup createDraftOrder.mutation.ts infrastructure

---
+++ backend-express/src/graphql/quotes/createDraftOrder.mutation.ts
@@ -0,0 +1,52 @@
+export const CREATE_DRAFT_ORDER_MUTATION_FULL = `
+  mutation draftOrderCreate($input: DraftOrderInput!) {
+    draftOrderCreate(input: $input) {
+      draftOrder {
+        id
+        name
+        invoiceUrl
+        totalPrice
+        customer {
+          id
+          email
+          firstName
+          lastName
+        }
+        lineItems(first: 10) {
+          edges {
+            node {
+              id
+              title
+              quantity
+              originalUnitPriceSet {
+                shopMoney {
+                  amount
+                }
+              }
+            }
+          }
+        }
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+export const CREATE_DRAFT_ORDER_MUTATION_MINIMAL = `
+      mutation CreateDraftOrder($input: DraftOrderInput!) {
+        draftOrderCreate(input: $input) {
+          draftOrder {
+            id
+            name
+            invoiceUrl
+          }
+          userErrors {
+            field
+            message
+          }
+        }
+      }
+    `;
