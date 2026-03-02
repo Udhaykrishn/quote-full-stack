ff1fcaf984afc4815a50fefc3e94a8fd808dcfcd
UdhayKrishn <krishnauday320@gmail.com>
2026-03-02 15:29:25 +0530

    feat(frontend): register form builder route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,12 +5,14 @@ import { Settings } from '../pages/Settings';
 import { Quotes } from '../pages/Quotes';
 import { DraftOrders } from '../pages/DraftOrders';
 import { Plans } from '../pages/Plans';
+import { FormBuilder } from '../pages/FormBuilder';
 
 export const AppRoutes: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/form-builder" element={<FormBuilder />} />
             <Route path="/quotes" element={<Quotes />} />
             <Route path="/draft-orders" element={<DraftOrders />} />
             <Route path="/plans" element={<Plans />} />
